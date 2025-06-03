2ee5024579eb0aa1cde80449bdb85870527ec7b9
JoseLuisGutierrezQ <101904595+JoseLuisGutierrezQ@users.noreply.github.com>
2025-06-03 16:31:49 -0500

    Add files via upload

---
+++ actividad 1.2/Node.java
@@ -0,0 +1,36 @@
+package actividad2;
+
+public class Node<T> {
+    private T data; 
+    private Node<T> next; 
+
+   
+    public Node (T data, Node<T> next){
+        this.data = data;
+        this.next = next;
+    }
+    
+   
+    public Node(T data){
+        this(data, null);
+    }
+
+   
+    public void setData(T data){
+        this.data = data;
+    }
+    
+    public T getData(){
+        return data;
+    }
+
+    
+    public void setNext(Node<T> next){
+        this.next = next;
+    }
+    
+    public Node<T> getNext(){
+        return next;
+    }
+}
+
